6849b2a7310668fe42dc4a5de2b22e43bebacdfe
Jesse Callahan <jessetylercallahan@gmail.com>
2019-12-03 17:14:57 -0800

    is the building blocks of a funky website

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $("button").click(function(event) {
+
+  event.preventDefault();
+var instrument = $("input:radio[name=instrumentname]:checked").val();
+var coffee = $("input:radio[name=coffeename]:checked").val();
+var demeanor = $("input:radio[name=demeanorname]:checked").val();
+var instrument = parseInt(instrument)
+var coffee = parseInt(coffee)
+var demeanor = parseInt(demeanor)
+
+
+// my reference
+// $(".wrestler").text(wrestler)
+// $(".coffee").text(coffee)
+if (instrument === 1 && coffee === 1) {
+  $('#answer1').show();
+}else {
+  $('#answer2').show();
+  }
+
+
+
+});
+});
