5d5e66f21b0b62a743b871755fe648e0a6a675e3
Student <example@example.com>
2019-12-04 18:35:12 -0800

    changed some of the logic, refactored my parseInt

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,10 @@ $(document).ready(function() {
   $("button").click(function(event) {
 
   event.preventDefault();
-var instrument = $("input:radio[name=instrumentname]:checked").val();
+var instrument = parseInt($("input:radio[name=instrumentname]:checked").val());
 var coffee = $("input:radio[name=coffeename]:checked").val();
 var demeanor = $("input:radio[name=demeanorname]:checked").val();
 var trip = $("input:radio[name=tripname]:checked").val();
-var instrument = parseInt(instrument)
 var coffee = parseInt(coffee)
 var demeanor = parseInt(demeanor)
 var trip = parseInt(trip)
@@ -15,17 +14,19 @@ var trip = parseInt(trip)
 // my reference
 // $(".instrument").text(istrument)
 // $(".coffee").text(coffee)
-if (instrument === 1 && coffee === 1 && demeanor === 1) {
+if (trip === 3) {
+  $('#answer3').show();
+  $('#answer1').hide();
+  $('#answer2').hide();
+} else if (instrument === 1 && coffee === 1 && demeanor === 1) {
   $('#answer1').show();
-}else {
-  $('#answer2').show();
-  }
-  if (trip === 3) {
-    $('#answer3').show();
-    $('#answer1').hide();
-    $('#answer2').hide();
-  }
-
+  $('#answer2').hide();
+  $('#answer3').hide();
+} else {
+  $("#answer2").show();
+  $('#answer1').hide();
+  $('#answer3').hide();
+}
 
 
 
